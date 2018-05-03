b79b874f1f5efa43f7d29b1c9289ddcd19ecc906
Sofiane MAKROUM <sofiane.makroum@gmail.com>
2018-05-03 22:56:16 +0200

    First commit

---
+++ test/controllers/tasks_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class TasksControllerTest < ActionDispatch::IntegrationTest
+  test "GET tasks. Should return two tasks as json" do
+    get tasks_path
+    assert_response :success
+    assert_equal "application/json; charset=utf-8", response.headers["Content-Type"]
+    assert_equal 2, response.parsed_body.count
+    assert_equal "Task1", response.parsed_body[0]["name"]
+    assert_equal tasks(:one).start, response.parsed_body[0]["start"]
+    assert_equal tasks(:one).end, response.parsed_body[0]["end"]
+    assert_equal "Task2", response.parsed_body[1]["name"]
+    assert_equal tasks(:two).start, response.parsed_body[1]["start"]
+    assert_equal tasks(:two).end, response.parsed_body[1]["end"]
+  end
+
+  test "GET task/1. Should return task 1 as json" do
+    @topology = tasks(:one)
+    get url_for(@topology)
+    assert_response :success
+    assert_equal "application/json; charset=utf-8", response.headers["Content-Type"]
+    assert_equal 1, response.parsed_body["id"]
+    assert_equal "Task1", response.parsed_body["name"]
+    assert_equal @topology.start, response.parsed_body["start"]
+    assert_equal @topology.end, response.parsed_body["end"]
+  end
+
+  test "POST tasks. Should save one task as json" do
+    post tasks_path, as: :json, params: {
+        name: "Task3",
+        start: DateTime.now,
+        end: DateTime.now + 4.hour
+    }
+    assert_response :success
+    assert_not_nil Task.find_by(name: "Task3")
+  end
+
+  test "PUT task/1. Should update task as json" do
+    @topology = tasks(:one)
+    assert_equal "Task1", @topology.name
+    put url_for(@topology), as: :json, params: {
+        name: "Task1updated",
+    }
+    assert_response :success
+    @task_after_update = Task.find(@topology.id)
+    assert_equal "Task1updated", @task_after_update.name
+  end
+
+end
--- test/controllers/topologies_controller_test.rb
+++ test/controllers/topologies_controller_test.rb
@@ -1,14 +1,40 @@
 require 'test_helper'
 
 class TopologiesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get topologies_index_url
+  test "GET topologies. Should return two tasks as json" do
+    get topologies_path
     assert_response :success
+    assert_equal "application/json; charset=utf-8", @response.headers["Content-Type"]
+    assert_equal 2, @response.parsed_body.count
+    assert_equal "Topology1", @response.parsed_body[0]["name"]
+    assert_equal "Topology2", @response.parsed_body[1]["name"]
   end
 
-  test "should get show" do
-    get topologies_show_url
+  test "GET topology/1. Should return topology 1 as json" do
+    @topology = topologies(:one)
+    get url_for(@topology)
     assert_response :success
+    assert_equal "application/json; charset=utf-8", response.headers["Content-Type"]
+    assert_equal 1, response.parsed_body["id"]
+    assert_equal "Topology1", response.parsed_body["name"]
   end
 
+  test "POST topologies. Should save one topology as json" do
+    post topologies_path, as: :json, params: {
+        name: "Topology3",
+    }
+    assert_response :success
+    assert_not_nil Topology.find_by(name: "Topology3")
+  end
+
+  test "PUT topology/1. Should update topology as json" do
+    @topology = topologies(:one)
+    assert_equal "Topology1", @topology.name
+    put url_for(@topology), as: :json, params: {
+        name: "Topology1updated",
+    }
+    assert_response :success
+    @topology_after_update = Topology.find(@topology.id)
+    assert_equal "Topology1updated", @topology_after_update.name
+  end
 end
